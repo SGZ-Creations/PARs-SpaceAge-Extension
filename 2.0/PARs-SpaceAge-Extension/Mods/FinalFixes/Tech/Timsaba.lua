46ab4a71fddae74aff097d1a3b1b97e1a5168838
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-07-29 06:31:55 +0200

    Coompatibilty

---
+++ 2.0/PARs-SpaceAge-Extension/Mods/FinalFixes/Tech/Timsaba.lua
@@ -0,0 +1,9 @@
+local Tech = data.raw["technology"]
+
+if mods["TIMSABA"] then
+	Tech["par-mech-armour-tech-1"].unit.count = 10000
+	Tech["par-mech-armour-tech-2"].unit.count = 10000
+	Tech["par-mech-armour-tech-3"].unit.count = 10000
+	Tech["par-mech-armour-tech-4"].unit.count = 10000
+	Tech["par-mech-armour-tech-5"].unit.count = 10000
+end
